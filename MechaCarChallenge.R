a02a203c74ef9ba87c891bd9699d0dc6612fc7d4
jmmadson <jmmadson@gmail.com>
2021-12-31 14:51:46 -0600

    Updating for Deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -3,3 +3,6 @@ MechaCar_df <- read.csv("MechaCar_mpg.csv")
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar_df)
 summary(lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + 
              +                ground_clearance + AWD, data = MechaCar_df))
+suspension_table <- read.csv(file="Suspension_Coil.csv",check.names = F, stringsAsFactors = F)
+total_summary <- suspension_table %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+lot_summary <- suspension_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
